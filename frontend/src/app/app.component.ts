7c8a78bb7adeb4d51e0db14dcd9915a74e0a71c5
Nycola Plaisance <nycola.plaisance@hotmail.com>
2025-03-28 19:07:51 -0400

    fix: Ran prettier formatter on frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: "app-root",
+    imports: [RouterOutlet],
+    templateUrl: "./app.component.html",
+    styleUrl: "./app.component.css"
 })
 export class AppComponent {
-  title = 'home';
+    title = "home";
 }
