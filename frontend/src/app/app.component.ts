0ab4fd28e107517b31d43b4c9bf793cb6490a87a
Nycola Plaisance <nPlaisance@recursyve.io>
2025-05-30 16:12:11 -0400

    feature: Added spartan ng

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
+
+import { HlmButtonDirective } from "@spartan-ng/helm/button";
 
 @Component({
     selector: "app-root",
-    imports: [RouterOutlet],
-    templateUrl: "./app.component.html",
-    styleUrl: "./app.component.css"
+    imports: [HlmButtonDirective],
+    templateUrl: "./app.component.html"
 })
 export class AppComponent {
     title = "home";
